ef2c99395fe5ab07929db9c2e2d8d76670d872c5
ad3rinto <aad3rinto@gmail.com>
2024-06-27 10:09:31 +0100

    Another NFI

--- main.py
+++ main.py
@@ -11,9 +11,20 @@ driver.get(site)
 # price = driver.find_element(By.CLASS_NAME, "a-price-whole").text
 title = driver.title
 # print(title)
-list_of_events = driver.find_elements(By.CSS_SELECTOR, ".menu li")
-print(list_of_events)
+list_of_events_dates = driver.find_elements(By.CSS_SELECTOR, ".event-widget .shrubbery .menu li time")
+list_of_events_titles = driver.find_elements(By.CSS_SELECTOR, ".event-widget .shrubbery .menu li a")
 
+shaped_dates_list = []
+for _ in list_of_events_dates:
+    shaped_dates_list.append(_.text)
+
+shaped_names_list = []
+for _ in list_of_events_titles:
+    shaped_names_list.append(_.text)
+
+full_list_dict = dict(zip(shaped_names_list, shaped_dates_list))
+
+print(full_list_dict)
 # closes particular tab
 # driver.close()
 #
