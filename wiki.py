f4b64422af8992019bd36f6af8f905df34fde965
A Aderinto <aad3rinto@gmail.com>
2024-07-02 14:50:48 +0100

    Still not working

--- wiki.py
+++ wiki.py
@@ -11,6 +11,10 @@ driver.get(site)
 article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a").text
 print(article_count)
 
-article_images = driver.find_element(By.CSS_SELECTOR, "img")
+
+the_path = "/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/div[3]/div/table/tbody/tr[1]/td/span/a/img"
+
+article_images = driver.find_element(By.XPATH, the_path).get_attribute("a")
 
 print(article_images)
+
